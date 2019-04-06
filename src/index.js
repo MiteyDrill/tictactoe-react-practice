f4b3bc35c8eadd06ea3b23270fbf84b3d10490e1
Benjamin Sanchez <Linkandcheezer@gmail.com>
2019-04-06 15:50:01 -0700

    <Body/> <Product/> Components and CSS Added some css for the body component added css for product component

--- src/index.js
+++ src/index.js
@@ -1,12 +1,113 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import bookImage from './images/book.png';
+import facebookLogo from './images/facebook.png';
+import instagramLogo from './images/instagram.png';
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
+class Product extends React.Component{
+  imageDivStyle = {
+    height: "100%"
+  }
+
+  imgStyle = {
+    width: "100%",
+    height: "80%"
+  }
+
+  productDivStyle = {
+    backgroundColor: "rgba(0,0,0,0)",
+    display: "grid",
+    gridTemplateColumns: "1fr 1fr 2fr",
+    gridRows: "1fr 1fr",
+    width: "100%",
+    height: "60px"
+  }
+
+  imgIconStyle={
+    backgroundColor: "white",
+    height: "70%",
+    justifySelf: "center",
+    alignSelf: "center",
+    borderRadius: "100%",
+  }
+
+  buttonStyle={
+    justifySelf: "center",
+    alignSelf: "center",
+    width: "50%",
+    height: "70%",
+    fontSize: "100%",
+  }
+
+  render(){
+    return(
+      <div style= {this.imageDivStyle}>
+        <img style = {this.imgStyle} src = {bookImage}/>
+        <div style={this.productDivStyle} >
+          <img style={this.imgIconStyle} src={facebookLogo}/>
+          <img style={this.imgIconStyle} src={instagramLogo}/>
+
+          <button style={this.buttonStyle}>Buy</button>
+        </div>
+      </div>
+    )
+  }
+}
+
+class Body extends React.Component{
+
+  render(){
+    return(
+      <main>
+        <Product className="product-container"/>
+        <Product className="product-container"/>
+        <Product className="product-container"/>
+        <Product className="product-container"/>
+        <Product className="product-container"/>
+        <Product className="product-container"/>
+      </main>
+    );
+  }
+}
+
+//can I make this a pure component?
+class Header extends React.Component{
+
+  renderNav(){
+    return(
+      <ul id='nav-list'>
+        <li key='1'>Home</li>
+        <li key='2'>Products</li>
+        <li key='3'>Cart</li>
+      </ul>
+    );
+  }
+
+  render(){
+    return(
+      this.renderNav()
+    )
+  };
+}
+
+class App extends React.Component{
+
+  render(){
+    return(
+      <div>
+        <Header className='header'></Header>
+        <Body> </Body>
+      </div>
+    )
+  }
+}
+
+// ========================================
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
